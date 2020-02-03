2416afb284bd7ca701678ff3bff151c8132cc335
Adam Massmann <akm2203@columbia.edu>
2020-02-03 17:44:14 -0500

    add ghc warnings for shakefile

--- Shakefile.hs
+++ Shakefile.hs
@@ -1,3 +1,9 @@
+{-# OPTIONS_GHC -Wall #-}
+{-# OPTIONS_GHC -Wcompat #-}
+{-# OPTIONS_GHC -Wincomplete-record-updates #-}
+{-# OPTIONS_GHC -Wincomplete-uni-patterns #-}
+{-# OPTIONS_GHC -Wredundant-constraints #-}
+
 import           Development.Shake
 import           Development.Shake.Command
 import           Development.Shake.FilePath

dd80709fdbf99f5b968cd5463861581d3d505974
Adam Massmann <akm2203@columbia.edu>
2020-02-13 09:51:06 -0500

    symlinks no good with git

--- Shakefile.hs
+++ Shakefile.hs
@@ -17,5 +17,5 @@ import           Development.Shake.Util
 main :: IO ()
 main = shakeArgs shakeOptions { shakeFiles = "_build" } $ do
   phony "clean" $ do
-    liftIO $ putStrLn "Cleaning files in _build"
+    putInfo "Cleaning files in _build"
     removeFilesAfter "_build" ["//*"]
